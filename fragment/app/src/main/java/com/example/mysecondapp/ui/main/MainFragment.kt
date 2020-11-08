5f42e544d0a9aa07f489284baf1f8b3f649a91e8
NishizawaNaoki <mino24zawawork@gmail.com>
2020-11-08 15:50:30 +0900

    fragmentでdataBindingを利用する方法

---
+++ fragment/app/src/main/java/com/example/mysecondapp/ui/main/MainFragment.kt
@@ -0,0 +1,35 @@
+package com.example.mysecondapp.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.mysecondapp.R
+import com.example.mysecondapp.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.viewmodel = viewModel
+    }
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+}
